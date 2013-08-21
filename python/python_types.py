d799fa978c3e35af6c37cced2b4e2c9458655659
randyliu <randyliu@foxmail.com>
2013-08-21 11:58:03 +0800

    fix bug

--- python/python_types.py
+++ python/python_types.py
@@ -1,66 +1,54 @@
-loadfile(lua_dir .. "lib/hotdata/syntactic_node.lua")()
+from hotpot.hotpot.hp_config import *
+from hotpot.hotdata.syntactic_node import *
+from document.walker import *
+import sys
+import os
 
-function print_val(val)
-	if val.type == E_SNVT_IDENTIFIER then
-		io.write(val.val.identifier)
-	elseif val.type == E_SNVT_CHAR then
-		io.write("'")
-		if(val.val.c == '\n')then
-			io.write('\\n')
-		else
-			io.write(val.val.c)
-		end
-		io.write("'")
-	elseif val.type == E_SNVT_DOUBLE then
-		io.write(val.val.d)
-	elseif val.type == E_SNVT_BOOL then
-		io.write(val.val.b)
-	elseif val.type == E_SNVT_STRING then
-		--需要处理字符串中的转义
-		io.write('"')
-		io.write(val.val.str)
-		io.write('"')
-	elseif val.type == E_SNVT_INT64 then
-		io.write(val.val.i64)
-	elseif val.type == E_SNVT_UINT64 then
-		io.write(val.val.ui64)
-	elseif val.type == E_SNVT_HEX_INT64 then
-		io.write(val.val.hex_i64)
-	elseif val.type == E_SNVT_HEX_UINT64 then
-		io.write(val.val.hex_ui64)
-	end
-end
+class PYTHON_TYPES(Walker):
+	def __init__(self, document, output_dir):
+		Walker.__init__(self, document)
+		self.output_dir = output_dir
 
-function on_const(const)
-	io.write(const.identifier)
-	io.write(' = ')
-	print_val(const.val)
-	io.write('\n')
-end
+	def get_val(self, val):
+		if val['type'] == E_SNVT_IDENTIFIER:
+			return val['val']['identifier']
+		elif val['type'] == E_SNVT_CHAR:
+			if(val['val']['c'] == '\n'):
+				return '\'\\n\''
+			else:
+				return '\'' + val['val']['c'] + '\''
+		elif val['type'] == E_SNVT_DOUBLE:
+			return val['val']['d']
+		elif val['type'] == E_SNVT_BOOL:
+			return val['val']['b']
+		elif val['type'] == E_SNVT_STRING:
+			return '\"' + val.val.str + '\"'
+		elif val['type'] == E_SNVT_INT64:
+			return val['val']['i64']
+		elif val['type'] == E_SNVT_UINT64:
+			return val['val']['ui64']
+		elif val['type'] == E_SNVT_HEX_INT64:
+			return val['val']['hex_i64']
+		elif val['type'] == E_SNVT_HEX_UINT64:
+			return val['val']['hex_ui64']	
+		pass
 
-function on_enum(enum)
-	for key, value in pairs(enum.enum_def_list) do
-		io.write(value.identifier .. ' = ')
-		print_val(value.val)
-		io.write('\n')
-	end
-end
+	def on_document_begin(self, document):
+		ofile_name = self.output_dir + '/' + document['file_name'].rstrip('.hd') + '.py'
+		self.fout = open(ofile_name, "w")
 
-function main(document)
-	if(ifiles ~= nil) then
-		for k, v in pairs(ifiles) do
-			print_line(0, 'loadfile("' .. v .. '")()')
-		end
-	end
+		self.print_file_prefix()
 
-	for key, value in pairs(document['definition_list']) do
-		if(value.type == E_DT_CONST)then
-			on_const(value.definition.de_const)
-		elseif(value.type == E_DT_ENUM)then
-			on_enum(value.definition.de_enum)
-		end
-	end
-end
+	def on_document_end(self, document):
+		self.fout.close()
 
-main(document)
+	def on_const(self, const):
+		self.print_line(0, const['identifier'] + ' = ' + str(self.get_val(const['val'])))
 
+	def on_enum(self, enum):
+		for value in enum['enum_def_list']:
+			self.print_line(0, value['identifier'] + ' = ' + str(self.get_val(value['val'])))
+
+def hpmain(document, output_dir):
+	cw = PYTHON_TYPES(document, output_dir)
+	return cw.walk()
