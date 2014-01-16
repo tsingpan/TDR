42c7ccf6b7ef70404c41b601ae67932fd6e9740f
randyliu <randyliu@foxmail.com>
2014-01-16 10:25:50 +0800

    优化数组的存储

--- source/generate/tlibc_reader.c
+++ source/generate/tlibc_reader.c
@@ -162,12 +162,11 @@ static TD_ERROR_CODE _on_struct(TLIBC_READER_GENERATOR *self, const ST_STRUCT *d
 				generator_print(&self->super, "\t\tif(read_tuint16(self, &data->%s_num) != E_TLIBC_NOERROR) goto ERROR_RET;\n", de_struct->field_list.field_list[i].identifier);
 				generator_print(&self->super, "\t\tif(read_field_end(self, \"%s_num\") != E_TLIBC_NOERROR) goto ERROR_RET;\n", de_struct->field_list.field_list[i].identifier);
 
-
-				generator_print(&self->super, "\t\tif(read_field_begin(self, \"%s\") != E_TLIBC_NOERROR) goto ERROR_RET;\n", de_struct->field_list.field_list[i].identifier);
+				
 				generator_print(&self->super, "\t\tfor(i = 0; i < %s; ++i)\n", de_struct->field_list.field_list[i].type.ct.vector_length);
 				generator_print(&self->super, "\t\t{\n");
 				generator_print(&self->super, "\t\t\tif(i == data->%s_num) break;\n", de_struct->field_list.field_list[i].identifier);
-				generator_print(&self->super, "\t\t\tif(read_vector_item_begin(self, i) != E_TLIBC_NOERROR) goto ERROR_RET;\n");
+				generator_print(&self->super, "\t\t\tif(read_field_begin(self, \"%s\") != E_TLIBC_NOERROR) goto ERROR_RET;\n", de_struct->field_list.field_list[i].identifier);
 				if(vector_type->st == E_ST_STRING)
 				{
 					generator_print(&self->super, "\t\t\tif(read_tstring(self, data->%s, %s) != E_TLIBC_NOERROR) goto ERROR_RET;\n", de_struct->field_list.field_list[i].identifier, vector_type->string_length);
@@ -183,11 +182,10 @@ static TD_ERROR_CODE _on_struct(TLIBC_READER_GENERATOR *self, const ST_STRUCT *d
 					}
 					generator_print(&self->super, ") != E_TLIBC_NOERROR) goto ERROR_RET;\n");
 				}
-				generator_print(&self->super, "\t\t\tif(read_vector_item_end(self, i) != E_TLIBC_NOERROR) goto ERROR_RET;\n");
+				generator_print(&self->super, "\t\t\tif(read_field_end(self, \"%s\") != E_TLIBC_NOERROR) goto ERROR_RET;\n", de_struct->field_list.field_list[i].identifier);
 				generator_print(&self->super, "\t\t}\n");				
-				generator_print(&self->super, "\t\tif(read_field_end(self, \"%s\") != E_TLIBC_NOERROR) goto ERROR_RET;\n", de_struct->field_list.field_list[i].identifier);
 
-				generator_print(&self->super, "\t\tif(read_vector_end(self) != E_TLIBC_NOERROR) goto ERROR_RET;\n");
+				generator_print(&self->super, "\t\tif(read_vector_end(self) != E_TLIBC_NOERROR) goto ERROR_RET;\n");				
 			}
 		}
 		else if(de_struct->field_list.field_list[i].type.type == E_SNT_SIMPLE)
--- tutorial/example.c
+++ tutorial/example.c
@@ -14,7 +14,7 @@
 #define MAX_BUFF_SIZE 1024
 
 static ST_DEFINITION g_definition;
-
+static ST_Parameters g_parameters ;
 void init()
 {
 	g_definition.type = E_DT_CONST;
@@ -26,6 +26,18 @@ void init()
 
 	g_definition.definition.de_const.val.type = E_SNVT_INT64;
 	g_definition.definition.de_const.val.val.i64 = 123321;
+
+
+
+	g_parameters.par_list_num = 2;
+	g_parameters.par_list[0].type.type = E_SNT_SIMPLE;
+	g_parameters.par_list[0].type.st = E_ST_UINT32;
+	snprintf(g_parameters.par_list[0].identifier, MAX_IDENTIFIER_LENGTH, "boy");
+
+	g_parameters.par_list[1].type.type = E_SNT_CONTAINER;
+	g_parameters.par_list[1].type.ct = E_CT_STRING;
+	snprintf(g_parameters.par_list[1].identifier, MAX_IDENTIFIER_LENGTH, "girl");
+
 }
 
 TLIBC_XML_READER xml_reader;
@@ -33,9 +45,6 @@ void test_xml()
 {
 	TLIBC_XML_WRITER xml_writer;	
 	int ret;
-
-	init();
-
 
 	xml_writer_init(&xml_writer, "t.xml");
 	ret = write_ST_DEFINITION(&xml_writer.super, &g_definition);
@@ -46,7 +55,20 @@ void test_xml()
 	xml_reader_init(&xml_reader, "t.xml");
 	ret = read_ST_DEFINITION(&xml_reader.super, &g_definition);
 }
+void test_xml2()
+{
+	TLIBC_XML_WRITER xml_writer;	
+	int ret;
 
+	xml_writer_init(&xml_writer, "test_xml2.xml");
+	ret = write_ST_Parameters(&xml_writer.super, &g_parameters);
+	xml_writer_fini(&xml_writer);
+
+	memset(&g_parameters, 0, sizeof(ST_Parameters));
+
+	xml_reader_init(&xml_reader, "test_xml2.xml");
+	ret = read_ST_Parameters(&xml_reader.super, &g_parameters);
+}
 void test_compact()
 {
 	
@@ -55,7 +77,11 @@ void test_compact()
 
 int main()
 {
+	init();
+
 	test_xml();
+
+	test_xml2();
 
 	test_compact();
 
