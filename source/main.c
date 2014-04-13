ed4976326017969709fd6badeed83b9606c30c00
小星星 <randyliu@foxmail.com>
2014-04-13 13:16:02 +0800

    用getopt分析命令行参数

--- source/main.c
+++ source/main.c
@@ -2,6 +2,7 @@
 #include "parse/parser.h"
 #include "language/language.h"
 #include "version.h"
+#include "getopt_long.h"
 
 
 #include "generate/generator_types_h.h"
@@ -25,20 +26,21 @@ void version()
 void usage()
 {
 	fprintf(stderr, "Usage: tdata [options] file\n\n");
-	fprintf(stderr, "Use tdata -help for a list of options\n");
+	fprintf(stderr, "Use tdata --help for a list of options\n");
 }
 
 void help()
 {
 	fprintf(stderr, "Usage: tdata [options] file.\n");
 	fprintf(stderr, "Options:\n");
-	fprintf(stderr, "  -version                     Print the compiler version.\n");	
-	fprintf(stderr, "  -o dir                       Set the output directory for gen-* packages.\n");
+	fprintf(stderr, "  -h, --help                   Display this information.\n");
+	fprintf(stderr, "  -v, --version                Print the compiler version.\n");
+	fprintf(stderr, "  -o dir, --output=dir         Set the output directory for gen-* packages.\n");
 	fprintf(stderr, "                               default: current directory.\n");
 	
-	fprintf(stderr, "  -I dir                       Add a directory to the list of directories.\n");
+	fprintf(stderr, "  -I dir, --include=dir        Add a directory to the list of directories.\n");
 	fprintf(stderr, "                               searched for include directives.\n");
-	fprintf(stderr, "  -gen STR                     Generate code with a generator.\n");
+	fprintf(stderr, "  -g STR, --gen=STR            Generate code with a generator.\n");
 	fprintf(stderr, "                               For each named %%.td input files, a generator will generate an output file only.\n");
 	fprintf(stderr, "Available generators:\n");
 	fprintf(stderr, "types_h        Generate the %%_types.h file for tlibc.\n");
@@ -47,9 +49,19 @@ void help()
 	fprintf(stderr, "writer_h       Generate the %%_writer.h file for tlibc.\n");
 	fprintf(stderr, "writer_c       Generate the %%_writer.c file for tlibc.\n");	
 	fprintf(stderr, "cs             Generate the %%.cs file for tlibcs.\n");
-	fprintf(stderr, "sql            Generate the %%.sql file for MySQL.\n");
-	
+	fprintf(stderr, "sql            Generate the %%.sql file for MySQL.\n");	
 }
+
+const char* const short_options = "vho:I:g:";
+
+const struct option long_options[] = {
+	{ "version",	0, NULL, 'v' },
+	{ "help"	,	0, NULL, 'h' },
+	{ "output"	,	1, NULL, 'o' },
+	{ "include"	,	1, NULL, 'I' },
+	{ "gen"	,		1, NULL, 'g' },
+	{ NULL,			0, NULL,  0  },
+};
 
 #define TD_MAX_GENERATOR 16
 
@@ -64,110 +76,87 @@ static generator_cs_t tlibc_cs_generator;
 
 static generator_t *generator_list[TD_MAX_GENERATOR];
 static uint32_t generator_num = 0;
-int main(int32_t argc, char **argv)
+int main(int32_t argc, char *argv[])
 {
+	int opt;
 	int32_t i;
 
 	parser_init(&parser);
-	for (i = 1; i < argc; ++i)
+	while((opt = getopt_long (argc, argv, short_options, long_options, NULL)) != -1)
 	{
-		char* arg;
-
-		arg = strtok(argv[i], " ");
-		if (arg[0] == '-' && arg[1] == '-')
+		switch(opt)
 		{
-			++arg;
-		}
-		if (strcmp(arg, "-help") == 0)
-		{
-			help();
-			goto ERROR_RET;
-		}
-		else if (strcmp(arg, "-version") == 0)
-		{
+		case 'v':
 			version();
-			goto ERROR_RET;
-		}
-		else if (strcmp(arg, "-I") == 0)
-		{
+			goto done;
+		case 'h':
+			help();
+			goto done;
+		case 'I':
 			if(g_include_dir_num >= INCLUDE_DIR_NUM)
 			{
 				fprintf(stderr, "Too many include directories.\n");
 				goto ERROR_RET;
 			}
-			g_include_dir[g_include_dir_num] = argv[++i];
-			if (g_include_dir[g_include_dir_num] == NULL)
-			{
-				fprintf(stderr, "Missing include directory specification\n");
-				usage();
-				goto ERROR_RET;
-			}
+			g_include_dir[g_include_dir_num] = optarg;
 			++g_include_dir_num;
-		}
-		else if (strcmp(arg, "-o") == 0)
-		{
-			g_output_dir = argv[++i];
-			if (g_output_dir == NULL)
-			{
-				fprintf(stderr, "Missing output directory specification\n");
-				usage();
-				goto ERROR_RET;
-			}
-		}
-		else if (strcmp(arg, "-gen") == 0)
-		{
-			arg = argv[++i];
-			
-			if(strcmp(arg, "types_h") == 0)
-			{
-				generator_types_h_init(&tlibc_types_generator, &parser.symbols);
-				generator_list[generator_num++] = &tlibc_types_generator.super;
-			}
-			else if(strcmp(arg, "reader_h") == 0)
-			{
-				generator_reader_h_init(&tlibc_reader_header_generator, &parser.symbols);
-				generator_list[generator_num++] = &tlibc_reader_header_generator.super;
-			}
-			else if(strcmp(arg, "reader_c") == 0)
-			{
-				generator_reader_c_init(&tlibc_reader_generator, &parser.symbols);
-				generator_list[generator_num++] = &tlibc_reader_generator.super;
-			}			
-			else if(strcmp(arg, "writer_h") == 0)
-			{
-				generator_writer_h_init(&tlibc_writer_header_generator, &parser.symbols);
-				generator_list[generator_num++] = &tlibc_writer_header_generator.super;
-			}
-			else if(strcmp(arg, "writer_c") == 0)
-			{
-				generator_writer_c_init(&tlibc_writer_generator, &parser.symbols);
-				generator_list[generator_num++] = &tlibc_writer_generator.super;
-			}
-			else if(strcmp(arg, "cs") == 0)
-			{
-				generator_cs_init(&tlibc_cs_generator, &parser.symbols);
-				generator_list[generator_num++] = &tlibc_cs_generator.super;
-			}
-			else if(strcmp(arg, "sql") == 0)
-			{
-				generator_sql_init(&tlibc_sql_generator, &parser.symbols);
-				generator_list[generator_num++] = &tlibc_sql_generator.super;
-			}
-		}		
-		else
-		{
 			break;
+		case 'o':
+			g_output_dir = optarg;
+			break;
+		case 'g':
+			{
+				const char *arg = optarg;
+				if(strcmp(arg, "types_h") == 0)
+				{
+					generator_types_h_init(&tlibc_types_generator, &parser.symbols);
+					generator_list[generator_num++] = &tlibc_types_generator.super;
+				}
+				else if(strcmp(arg, "reader_h") == 0)
+				{
+					generator_reader_h_init(&tlibc_reader_header_generator, &parser.symbols);
+					generator_list[generator_num++] = &tlibc_reader_header_generator.super;
+				}
+				else if(strcmp(arg, "reader_c") == 0)
+				{
+					generator_reader_c_init(&tlibc_reader_generator, &parser.symbols);
+					generator_list[generator_num++] = &tlibc_reader_generator.super;
+				}			
+				else if(strcmp(arg, "writer_h") == 0)
+				{
+					generator_writer_h_init(&tlibc_writer_header_generator, &parser.symbols);
+					generator_list[generator_num++] = &tlibc_writer_header_generator.super;
+				}
+				else if(strcmp(arg, "writer_c") == 0)
+				{
+					generator_writer_c_init(&tlibc_writer_generator, &parser.symbols);
+					generator_list[generator_num++] = &tlibc_writer_generator.super;
+				}
+				else if(strcmp(arg, "cs") == 0)
+				{
+					generator_cs_init(&tlibc_cs_generator, &parser.symbols);
+					generator_list[generator_num++] = &tlibc_cs_generator.super;
+				}
+				else if(strcmp(arg, "sql") == 0)
+				{
+					generator_sql_init(&tlibc_sql_generator, &parser.symbols);
+					generator_list[generator_num++] = &tlibc_sql_generator.super;
+				}
+			}
+			break;
+		default:
+			goto ERROR_RET;
 		}
 	}
-
+	
 	if(generator_num == 0)
 	{
-		fprintf(stderr, "Missing -gen types\n");
-		help();
+		fprintf(stderr, "Missing --gen=types\n");
+		usage();
 		goto ERROR_RET;
 	}
 
-	for(; i < argc; ++i)
+	for(i = optind; i < argc; ++i)
 	{
 		if(parser_parse(&parser, argv[i], generator_list, generator_num) != E_TD_NOERROR)
 		{
@@ -175,6 +164,7 @@ int main(int32_t argc, char **argv)
 		}
 	}
 
+done:
 	return 0;
 ERROR_RET:
 	return 1;
