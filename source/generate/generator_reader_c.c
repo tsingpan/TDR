304c8718471d32b876975b086fa0c2325801cea9
小星星 <randyliu@foxmail.com>
2014-10-08 17:06:33 +0800

    改名

--- source/generate/generator_reader_c.c
+++ source/generate/generator_reader_c.c
@@ -32,7 +32,7 @@ static error_code_t on_document_begin(generator_t *super, const YYLTYPE *yylloc,
 	generator_printline(super, 0, "#include <stdint.h>");
 	generator_printline(super, 0, "#include <stdbool.h>");
 	generator_printline(super, 0, "#include <string.h>");
-	generator_printline(super, 0, "#include \"tlibcdef.h\"");
+	generator_printline(super, 0, "#include \"tlibc_define.h\"");
 
 	generator_printline(super, 0, "");
 	generator_printline(super, 0, "");
