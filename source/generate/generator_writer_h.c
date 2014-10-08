2a99d5e76f4e2674c51ebdc012c9d0d5d123eab7
小星星 <randyliu@foxmail.com>
2014-10-08 15:48:33 +0800

    更改头文件路径

--- source/generate/generator_writer_h.c
+++ source/generate/generator_writer_h.c
@@ -35,7 +35,7 @@ static error_code_t on_document_begin(generator_t *super, const YYLTYPE *yylloc,
 
 	generator_printline(super, 0, "");
 	generator_printline(super, 0, "#include <stdint.h>");
-	generator_printline(super, 0, "#include \"protocol/tlibc_abstract_writer.h\"");
+	generator_printline(super, 0, "#include \"tlibc_abstract_writer.h\"");
 	generator_printline(super, 0, "#include \"tlibc_error_code.h\"");
 	
 	//包含types的头文件
